4bf7f8f75dd99753c15863219c11d673e475abc4
ahalekelly <ahalekelly@gmail.com>
2017-12-16 07:40:46 -0800

    Bug fixes and performance improvements

--- springerSim.py
+++ springerSim.py
@@ -10,21 +10,28 @@ import matplotlib.pyplot as plt
 plt.ioff()
 fig = plt.figure(figsize=(18,12), dpi=600)
 plt.title('Springer Simulation')
-plt.xlabel('Dart travel (inches)')
 plt.ylabel('fps / mm / MPa')
 
 # Settings
-#barrelLength = 0.321754 # m
+
+# Caliburn
+barrelLength = 0.321754 # m 
 barrelLength = 3.5
 plungerDiameter = 0.03495 # m
 barrelDiameter = 0.0127 # m
 plungerDraw = 0.15138
 precompression = 0.040818
-springRate = 679.651333723 # N/m
-plungerMass = 0.04 # kg
-plungerFriction = 0.05 #kg
+#springRate = 679.651333723 # N/m k26
+springForce = 13.3*9.8 # N at starting distance
+springMass = 0.0284
+plungerRodMass = 0.04 # kg
+plungerFriction = 0.5 # N
 deadspace = 0.00000792152 # cubic meters
 
+# Sentinel
+#plungerMass = 0.03
+plungerDiameter = 1.25*.0254
+plungerDraw = 3.25*.0254
 
 # Nite Finder
 #plungerMass = 0.0001
@@ -36,7 +43,7 @@ deadspace = 0.00000792152 # cubic meters
 #springRate = 2*9.8/plungerDraw
 
 dartMass = 0.0012 # kg
-dartFriction = 0.25 # kg
+dartFriction = 2.5 # N
 
 dt = 0.00001
 maxTime = 0.1
@@ -60,9 +67,11 @@ def simulate():
     plungerRadius = plungerDiameter/2
     barrelRadius = barrelDiameter/2
     plungerStart = precompression + plungerDraw
+    plungerMass = plungerRodMass + springMass/2
+    springRate = springForce/plungerStart # N/m
+    print(plungerStart, springRate)
     plungerForce = plungerStart * springRate - plungerFriction
-    airVolume = deadspace + (plungerStart-precompression) * pi * plungerRadius**2
-    
+    airVolume = deadspace + (plungerStart-precompression) * pi * plungerRadius**2    
     
     times[0] = 0
     pressures[0] = 0
@@ -81,6 +90,8 @@ def simulate():
         if plungerPositions[i-1] > precompression:
             plungerVelocities[i] = plungerVelocities[i-1] + plungerForce/plungerMass*dt
             plungerPositions[i] = max(precompression, plungerPositions[i-1] - plungerVelocities[i]*dt)
+            if plungerPositions[i] == precompression:
+                print("plunger hit end at", dartPositions[i-1]*ft*12)
         else:
             plungerVelocities[i] = 0
             plungerPositions[i] = precompression
@@ -115,17 +126,20 @@ def simulate():
     print(clock()-timestart, "s runtime")
     print(dartVelocities[i]*ft, "fps in", times[i], "s")
 
-#    xAxis = times
+    xAxis = times
+    plt.xlabel('Time elapsed (seconds)')
     xAxis = dartPositions*ft*12
-    plt.plot(xAxis, dartVelocities*ft, label="velocity, max "+str(int(np.amax(dartVelocities[0:i])*ft))+"fps")
+    plt.xlabel('Dart travel (inches)')
+    plt.plot(xAxis, dartVelocities*ft, label="velocity, max "+str(int(np.amax(dartVelocities[0:i])*ft))+"fps @ "+str(round(dartPositions[np.argmax(dartVelocities[0:i])]*ft*12, 1))+" in")
     plt.plot(xAxis, (plungerPositions-precompression)*1000, label="plunger position (mm)")
 #    plt.plot(xAxis, dartPositions*200, label="plunger position (mm)")
     plt.plot(xAxis, pressures*atm/1000, label="pressure (MPa)")
     
+springForce=8*9.8
 simulate()
-
-dartFriction = 0 # kg
-
+springForce=10*9.8
+simulate()
+springForce=13*9.8
 simulate()
 
 #plt.ylim(0,80)
